cc323ad7e9531bf54002e883e4015dec71ab8cc1
Justin Azoff <jazoff@illinois.edu>
2014-09-19 14:10:27 -0400

    refactor

---
+++ bhr_client/block_manager.py
@@ -0,0 +1,50 @@
+import signal
+import time
+class DummyStdoutBlocker:
+    def __init__(self):
+        pass
+
+    def block_many(self, records):
+        for r in records:
+            print time.ctime(), "block", r['cidr']
+
+    def unblock_many(self, records):
+        for r in records:
+            print time.ctime(), "unblock", r['block']['cidr']
+
+class BlockManger:
+    def __init__(self, client, blocker):
+        self.client = client
+        self.blocker = blocker
+
+    def do_block(self):
+        records = self.client.get_block_queue()
+        if records:
+            self.blocker.block_many(records)
+            self.client.set_blocked(records)
+        return bool(records)
+
+    def do_unblock(self):
+        records = self.client.get_unblock_queue()
+        if records:
+            self.blocker.unblock_many(records)
+            self.client.set_unblocked(records)
+        return bool(records)
+
+    def run_once(self):
+        did = self.do_block()
+        did = did or self.do_unblock()
+        return did
+
+    def run(self):
+        x = 0
+        while True:
+            signal.alarm(30)
+            did = self.do_block()
+            if x % 10 == 0:
+                did = did or self.do_unblock()
+            if not did:
+                x += 1
+                time.sleep(2)
+            else:
+                time.sleep(.01)
--- bhr_client/run.py
+++ bhr_client/run.py
@@ -1,11 +1,14 @@
 from bhr_client.rest import Client
+from bhr_client.block_manager import BlockManager, DummyStdoutBlocker
 import sys
 
 def main():
     ident = sys.argv[1]
 
     c = Client(ident)
-    c.run()
+    blocker = DummyStdoutBlocker()
+    m = BlockManager(client, blocker)
+    m.run()
 
 if __name__ == "__main__":
     main()
