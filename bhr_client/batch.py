4822df8f182148afe05a69f627ab1a6970508c30
Edward Delaporte <delaport@illinois.edu>
2021-02-25 11:41:05 -0600

    Fixes for pull request review.

--- bhr_client/batch.py
+++ bhr_client/batch.py
@@ -1,17 +1,20 @@
 #! /usr/local/env python
 from bhr_client.rest import login_from_env
 import argparse
-import csv
 import time
 
-bhr = login_from_env()
-parser = argparse.ArgumentParser()
-parser.add_argument('--file', help="Filename with the IPs (expect 1 per line)", required=True)
-parser.add_argument('--time', help="block time in seconds", required=True)
-parser.add_argument('--why', help="reason for the block", required=True)
+def main():
+    bhr = login_from_env()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--file', help="Filename with the IPs (expect 1 per line)", required=True)
+    parser.add_argument('--time', help="block time in seconds", required=True)
+    parser.add_argument('--why', help="reason for the block", required=True)
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-with open(args.file,"r") as f:
-    for line in f.readlines():
-        bhr.block(cidr=line, source="BHR Client Batch", why=args.why, duration=args.time)
+    with open(args.file,"r") as f:
+        for line in f:
+            bhr.block(cidr=line, source="BHR Client Batch", why=args.why, duration=args.time)
+
+if __name__ == "__main__":
+    main()
